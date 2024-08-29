c60bf808f43e778789f9445fd5781932c37120ab
7shi <7shi@live.jp>
2024-08-30 06:20:22 +0900

    use <stdint.h> and types

--- net/_main.c
+++ net/_main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include <string.h>
 #include <sys/time.h>
 
 #define ETH_ARP 0x0806
 
-extern void hexdump(const char *, int);
-extern void hexadump(const char *, int);
+extern void hexdump(const uint8_t *, int);
+extern void hexadump(const uint8_t *, int);
 
-void hexdump2(const char *buf, int len) {
+void hexdump2(const void *buf, int len) {
     int i, f;
-    const unsigned char *b = buf;
+    const uint8_t *b = buf;
     printf("==== Dump ====\n");
     for (i = 0; i < len; i++, b++) {
         if (i) printf(i & 15 ? " " : "\n");
@@ -20,18 +21,18 @@ void hexdump2(const char *buf, int len) {
     printf("\n");
 }
 
-unsigned short read16be(const void *buf) {
-    unsigned char *p = (unsigned char *)buf;
+uint16_t read16be(const void *buf) {
+    const uint8_t *p = (const uint8_t *)buf;
     return (p[0] << 8) | p[1];
 }
 
-unsigned read32be(const void *buf) {
-    unsigned char *p = (unsigned char *)buf;
+uint32_t read32be(const void *buf) {
+    const uint8_t *p = (const uint8_t *)buf;
     return (p[0] << 24) | (p[1] << 16) | (p[2] << 8) | p[3];
 }
 
-int dump_packet(const char *buf, int size) {
-    int ret = read16be(&buf[12]);
+int dump_packet(const uint8_t *buf, uint32_t size) {
+    uint16_t ret = read16be(&buf[12]);
     if (ret == ETH_ARP) {
         printf("==== Packet (ARP) ====\n");
         hexadump(buf, size - 14);
@@ -42,27 +43,26 @@ int dump_packet(const char *buf, int size) {
     return ret;
 }
 
-int xinu_read(int did, char *buf, unsigned size) {
+int xinu_read(int did, uint8_t *buf, uint32_t size) {
     printf("read(%d, %p, %u)\n", did, buf, size);
     return 0;
 }
 
 extern void arp_packet(
-    void *apkt, unsigned short op,
-    void *dst_mac, unsigned dst_ip,
-    void *src_mac, unsigned src_ip);
+    void *apkt, uint16_t op,
+    void *dst_mac, uint32_t dst_ip,
+    void *src_mac, uint32_t src_ip);
 extern void arp_in(void *);
 
-extern unsigned char NetData[48];
+extern uint8_t NetData[48];
 
-int xinu_write(int did, char *buf, unsigned size) {
+int xinu_write(int did, const uint8_t *buf, uint32_t size) {
     printf("write(%d, %p, %u)\n", did, buf, size);
     hexdump2(buf, size);
     if (did == 2 && dump_packet(buf, size) == ETH_ARP && read16be(&buf[14]) == 1) {
         const int apkt_size = 42;
-        unsigned char *apkt = malloc(apkt_size);
-        long mac = ((long)apkt) & 0xffffffffffffL;
-        unsigned ips = read32be(&buf[0x26]);
+        uint8_t *apkt = (uint8_t *)malloc(apkt_size);
+        uint64_t mac = ((long)apkt) & 0xffffffffffffL;
         arp_packet(apkt, 2, &NetData[33], *(unsigned *)NetData, &mac, read32be(&buf[0x26]));
         hexdump2(apkt, apkt_size);
         dump_packet(apkt, apkt_size);
@@ -173,7 +173,7 @@ void xinu_resume(int id) {
     printf("resume(%d)\n", id);
 }
 
-int xinu_control(int a, int b, long c, long d) {
+int xinu_control(int a, int b, intptr_t c, intptr_t d) {
     printf("control(%d, %d, %p, %p)\n", a, b, c, d);
     // ETHER0, ETH_CTRL_GET_MAC, (intptr)NetData.ethucast, 0
     if (a == 2 && b == 1) {
@@ -184,7 +184,7 @@ int xinu_control(int a, int b, long c, long d) {
     return 0;
 }
 
-int xinu_send(int pid, unsigned msg) {
+int xinu_send(int pid, uint32_t msg) {
     printf("send(%d, %u)\n", pid, msg);
     return 0;
 }
@@ -203,9 +203,9 @@ int xinu_signal(int sid) {
 #define ICMP_ECHOREQST 8
 
 extern void net_init();
-extern int dnslookup(const char *, unsigned *);
-extern int icmp_register(unsigned);
-extern int icmp_send(unsigned, unsigned short, unsigned short, unsigned short, char *, int);
+extern int dnslookup(const char *, uint32_t *);
+extern int icmp_register(uint32_t);
+extern int icmp_send(uint32_t, uint16_t, uint16_t, uint16_t, uint8_t *, int);
 extern int icmp_release(int);
 
 int main(int argc, char *argv[])
@@ -214,13 +214,13 @@ int main(int argc, char *argv[])
     net_init();
 
     const char *myips = "192.168.0.81";
-    unsigned myipaddr;
+    uint32_t myipaddr;
     result = dnslookup(myips, &myipaddr);
     printf("dnslookup(\"%s\", %p) => %d\n", myips, &myipaddr, result);
     memcpy(NetData, &myipaddr, 4);
 
     const char *ips = "192.168.0.100";
-    unsigned ipaddr;
+    uint32_t ipaddr;
     result = dnslookup(ips, &ipaddr);
     printf("dnslookup(\"%s\", %p) => %d\n", ips, &ipaddr, result);
     printf("Pinging %d.%d.%d.%d\n",
@@ -243,7 +243,7 @@ int main(int argc, char *argv[])
 
     int seq = 0;
     int nextval = seq;
-    char buf[56];
+    uint8_t buf[56];
     for (int i = 0; i<sizeof(buf); i++) {
         buf[i] = 0xff & nextval++;
     }
